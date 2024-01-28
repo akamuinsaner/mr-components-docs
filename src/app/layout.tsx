38e4bcab08caa8b3790c48fbec129e79a5792ebc
wangshuai <15108387504@163.com>
2024-01-28 16:14:04 +0800

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import App from './_app';
+import { Stack } from '@mui/material';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,6 +21,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
          {children}
+         <Stack
+            className='footer'
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+         >
+           <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2024045238号-1</a>
+         </Stack>
         </body>
     </html>
   )
